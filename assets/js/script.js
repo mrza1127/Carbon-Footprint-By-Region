74b908fcce7be8f4a8d344d67c02d43c99ffd44b
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-06-25 16:33:42 -0500

    adding 2nd api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
 const carbonFootPrintForm = document.getElementById('carbonFootPrint');
+const clothingfootprintForm = document.getElementById('clothingfootprint');
 const resultsScreen = document.getElementById('results');
 const yourEmissionProgerss = document.getElementById('yourEmissionProgress');
 const yourEmission = document.getElementById('yourEmission');
@@ -7,13 +8,21 @@ var selectedState = '';
 var activities = '';
 var unit = '';
 var powerUsage = '';
-const request_url = 'https://www.carboninterface.com/api/v1/estimates';
-const api_token = 'Eiv9kGXI9G1VxlTKFIzCQ';
-const climatiq_url = 'https://beta3.api.climatiq.io/estimate';
-const climatiq_token = 'E7TAM9S8DR48YCJXKWRWNY09TPXM';
+var moneyUnit ='';
+var moneyEntered ='';
+const electricity_request_url = 'https://www.carboninterface.com/api/v1/estimates';
+const electricity_api_token = 'Eiv9kGXI9G1VxlTKFIzCQ';
+const clothing_footwear_request_url = 'https://beta3.api.climatiq.io/estimate';
+const clothing_footwear_token = 'E7TAM9S8DR48YCJXKWRWNY09TPXM';
 
 
 resetForm();
+
+function resetForm() {
+    carbonFootPrintForm.reset();
+    resultsScreen.style.display = 'none';
+    clothingfootprintForm.style.display = 'none';
+}
 
 function submitCarbonForm(event) {
     event.preventDefault();
@@ -22,12 +31,10 @@ function submitCarbonForm(event) {
     unit = carbonFootPrintForm.elements['unit'].value;
     powerUsage = carbonFootPrintForm.elements['powerUsage'].value;
     calculateCarbonFootPrint();
+    displayClothingForm();
+    
 };
 
-function resetForm() {
-    carbonFootPrintForm.reset();
-    resultsScreen.style.display = 'none';
-}
 
 function calculateCarbonFootPrint() {
     let request_body = {
@@ -38,10 +45,51 @@ function calculateCarbonFootPrint() {
         "electricity_value": parseInt(powerUsage)
     }
 
-    fetch(request_url, {
+    fetch(electricity_request_url, {
         method: 'POST',
         headers: {
-            "Authorization": 'Bearer ' + api_token,
+            "Authorization": 'Bearer ' + electricity_api_token,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(request_body)
+    })
+        .then((res) => res.json())
+        .then(data => saveElectricityResult(data.data.attributes.carbon_mt));
+
+};
+
+
+
+function saveElectricityResult(carbon_mt){
+    localStorage.setItem('electricityResults', carbon_mt);
+} 
+
+function displayClothingForm() {
+    clothingfootprintForm.style.display = 'block';
+    carbonFootPrintForm.style.display = 'none';
+}
+
+function submitClothingForm(event) {
+    event.preventDefault();
+    moneyUnit = clothingfootprintForm.elements['moneyDropdown'].value;
+    moneyEntered = clothingfootprintForm.elements['dollarAmount'].value;
+    calculateClothingFootWearPrint();
+    displayClothingForm();
+};
+
+function calculateClothingFootWearPrint() {
+    let request_body = {
+        "emission_factor": "consumer_goods-type_clothing",
+        "parameters": {
+			"money": parseInt(moneyEntered),
+			"money_unit": moneyUnit
+        }
+    }
+
+    fetch(clothing_footwear_request_url, {
+        method: 'POST',
+        headers: {
+            "Authorization": 'Bearer ' + clothing_footwear_token,
             "Content-Type": "application/json"
         },
         body: JSON.stringify(request_body)
@@ -52,12 +100,11 @@ function calculateCarbonFootPrint() {
 };
 
 function displayResults(results) {
-    yourEmissionProgress.value = results.data.attributes.carbon_mt; 
-    yourEmission.innerHTML = results.data.attributes.carbon_mt;
-    resultsScreen.style.display = 'block';
-    saveElectricityResult(results.data.attributes.carbon_mt);
-}
 
-function saveElectricityResult(carbon_mt){
-    localStorage.setItem('electricityResults', carbon_mt);
-} 
+    var electricityEmmison = parseFloat(localStorage.getItem('electricityResults'));
+    var clothingFootwearEmission = results.constituent_gases.co2e_total / 100;
+    var totalEmission = electricityEmmison + clothingFootwearEmission;
+    yourEmissionProgress.value =  totalEmission.toString();
+    yourEmission.innerHTML = totalEmission.toString();;
+    resultsScreen.style.display = 'block';
+}
